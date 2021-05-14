e3d33a9e2bf2700e6bbbdc86137e7956cc88b1d5
Jacques Ikot <jacquesikot@gmail.com>
2021-05-14 11:27:36 +0100

    feat(index): add erc20 ABI

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const OneSplitAbi = require('./oneSplitAuditFull.json');
+const erc20Abi = require('erc-20-abi');
+
+const splitExchanges = require('./splitExchanges');
 
 const weiEthDecimal = 18;
 
@@ -10,69 +13,40 @@ const provider = new Web3.providers.HttpProvider(
 
 const web3 = new Web3(provider);
 
-const fromTokenAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
-const toTokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+const fromAddress = '0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c'; // Addres of random eth account with enough  ETh and DAI
+const toTokenAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'; // Address of Eth
+const fromTokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f'; // Address of DAI
 const amount = 1;
+const OneSplitAddress = '0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E'; // Address of OneSplit smart contract
+const amountToSwap = 1690;
+const amountToSwapWei = new BigNumber(amountToSwap).shiftedBy(weiEthDecimal);
 
-const OneSplitContract = new web3.eth.Contract(
-  OneSplitAbi,
-  '0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E'
-);
+const OneSplitContract = new web3.eth.Contract(OneSplitAbi, OneSplitAddress);
+var DaiContract = new web3.eth.Contract(erc20Abi, fromTokenAddress);
 
-let splitExchanges = [
-  'Uniswap',
-  'Kyber',
-  'Bancor',
-  'Oasis',
-  'Curve Compound',
-  'Curve USDT',
-  'Curve Y',
-  'Curve Binance',
-  'Curve Synthetix',
-  'Uniswap Compound',
-  'Uniswap CHAI',
-  'Uniswap Aave',
-  'Mooniswap',
-  'Uniswap V2',
-  'Uniswap V2 ETH',
-  'Uniswap V2 DAI',
-  'Uniswap V2 USDC',
-  'Curve Pax',
-  'Curve renBTC',
-  'Curve tBTC',
-  'Dforce XSwap',
-  'Shell',
-  'mStable mUSD',
-  'Curve sBTC',
-  'Balancer 1',
-  'Balancer 2',
-  'Balancer 3',
-  'Kyber 1',
-  'Kyber 2',
-  'Kyber 3',
-  'Kyber 4',
-];
-
-OneSplitContract.methods
-  .getExpectedReturn(
-    fromTokenAddress,
-    toTokenAddress,
-    new BigNumber(amount).shiftedBy(weiEthDecimal),
-    100,
-    0
-  )
-  .call({}, (err, res) => {
-    if (err) console.error(err);
-    console.log(`
-        from: ${fromTokenAddress}
-        to: ${toTokenAddress}
-        amount: ${amount}
-        returnAmount: ${new BigNumber(res.returnAmount).shiftedBy(
-          -weiEthDecimal
-        )}
-    `);
-    for (let i = 0; i < res.distribution.length; i++) {
+async function getExpectedReturn() {
+  await OneSplitContract.methods
+    .getExpectedReturn(
+      toTokenAddress,
+      fromTokenAddress,
+      new BigNumber(amount).shiftedBy(weiEthDecimal),
+      100,
+      0
+    )
+    .call({}, (err, res) => {
+      if (err) console.error(err);
       console.log(`
-        ${splitExchanges[i] + ' :' + res.distribution[i]}`);
-    }
-  });
+          from: ${fromTokenAddress}
+          to: ${toTokenAddress}
+          amount: ${amount}
+          returnAmount: ${new BigNumber(res.returnAmount)
+            .shiftedBy(-weiEthDecimal)
+            .toString()}
+      `);
+      splitExchanges.forEach((dex, i) => {
+        console.log(`${dex}: ${res.distribution[i]}%`);
+      });
+    });
+}
+
+getExpectedReturn();
